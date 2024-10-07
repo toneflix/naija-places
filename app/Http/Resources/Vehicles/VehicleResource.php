4abe9eebc395f8d990d075ad026720bbeba278b8
3m1n3nc3 <mygames.ng@gmail.com>
2024-10-07 16:48:30 +0100

    Add fuel_type to VehicleResource

--- app/Http/Resources/Vehicles/VehicleResource.php
+++ app/Http/Resources/Vehicles/VehicleResource.php
@@ -29,6 +29,14 @@ class VehicleResource extends JsonResource
             "class" => $this->car_class,
             "engine" => $this->when($with->contains('engine'), fn() => new EngineResource($this->engine), new stdClass()),
             "mileage" => $this->when($with->contains('mileage'), fn() => new MileageResource($this->mileage), new stdClass()),
+            "fuel_type" => $this->whenLoaded('engine', function () {
+                return data_get([
+                    '' => 'electric',
+                    'diesel' => 'diesel',
+                    'petrol' => 'gasoline',
+                    'gasoline' => 'gasoline',
+                ], str($this->engine->engine_type ?: '')->lower(), $this->engine->engine_type);
+            }),
             "createdAt" => $this->created_at,
             "updatedAt" => $this->updated_at,
         ];
